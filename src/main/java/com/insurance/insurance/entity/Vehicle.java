84125f4369cf247cf45496af2063d67ba8775e0b
chris Taylor <ctay456a@gmail.com>
2025-06-03 23:00:58 -0400

    Create Vehicle.java

---
+++ src/main/java/com/insurance/insurance/entity/Vehicle.java
@@ -0,0 +1,23 @@
+// src/main/java/com/insurance/insurance/entity/Vehicle.java
+package com.insurance.insurance.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String make;
+    private String model;
+    private int year;
+    private String registrationNumber;
+}
